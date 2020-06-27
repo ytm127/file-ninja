33cfa9d56db03cd4ffc90a6bce908b3f1752ab2d
Thomas Morita <>
2020-06-26 20:17:48 -0400

    lil style updaets wip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Upload} from "./components/Upload"
-// import './App.css';
+import { Upload } from './components/Upload';
+import './App.css';
+import './skeleton.css';
 
 function App() {
-  return (
-    <div className="App">
-    <div style={{textAlign:"center"}}>
-    <h1>File Ninja</h1>
-      <h4>The sneaky, ephemeral file share</h4>
-      <Upload />
-    </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div style={{ textAlign: 'center' }}>
+        <br/>
+				<div className='title-container'><span className="fold"></span><h1>File Ninja</h1> </div>
+				<h4>The sneaky, ephemeral file share</h4>
+        <div>
+					<img src="https://img.icons8.com/windows/32/000000/ninja-head.png" />
+				</div>
+				<Upload />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,28 +1,28 @@
-import React, { useState, createRef } from "react";
-import axios from "axios";
+import React, { useState, createRef } from 'react';
+import axios from 'axios';
 
-export const Upload = props => {
-  const [keyLink, setKeyLink] = useState("");
-  const fileRef = createRef();
+export const Upload = (props) => {
+	const [ keyLink, setKeyLink ] = useState('');
+	const fileRef = createRef();
 
-  const handleSubmit = e => {
-    setKeyLink("");
-    const data = new FormData();
-    data.append("file", fileRef.current.files[0]);
-    axios
-      .post("https://file.io", data)
-      .then(({ data }) => setKeyLink(data.link));
-    e.preventDefault();
-  };
+	const handleSubmit = (e) => {
+		setKeyLink('');
+		const data = new FormData();
+		data.append('file', fileRef.current.files[0]);
+		axios.post('https://file.io', data).then(({ data }) => setKeyLink(data.link));
+		e.preventDefault();
+	};
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" ref={fileRef} />
-        <input type="submit" value="Submit" />
-      </form>
-      <br />
-      {keyLink}
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<input type="file" ref={fileRef} />
+				</div>
+				<input type="submit" value="Submit" className="button-primary" />
+			</form>
+			<br />
+			{keyLink}
+		</div>
+	);
 };
